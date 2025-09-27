36abde417f45cf1ba173e0633528b76376b59cd9
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-09-27 09:46:07 -0300

    Ajustes para Pt

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -4,6 +4,7 @@ import type { Locale } from "./i18n-config";
 // We enumerate all dictionaries here for better linting and typescript support
 // We also get the default import for cleaner types
 const dictionaries = {
+  pt: () => import("./dictionaries/pt.json").then((module) => module.default),
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   de: () => import("./dictionaries/de.json").then((module) => module.default),
   cs: () => import("./dictionaries/cs.json").then((module) => module.default),
